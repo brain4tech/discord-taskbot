0118d1a8ec633ec56b18368a77d312c6a00264b0
Brain4Tech <Jefta.deLeeuw@web.de>
2022-09-20 14:31:49 +0200

    add persistence api to client

--- discord_taskbot/utils/client_subclass.py
+++ discord_taskbot/utils/client_subclass.py
@@ -5,12 +5,16 @@ Custom subclass of discord.Client.
 import discord
 from discord import app_commands
 from typing import Any
+from discord_taskbot.components.persistence import PersistenceAPI
 
 class TaskBot(discord.Client):
     def __init__(self, *, intents: discord.Intents, **options: Any) -> None:
         super().__init__(intents=intents, **options)
 
         self.tree = app_commands.CommandTree(self)
+
+        self.db = PersistenceAPI()
+        self.db.startup()
     
     async def setup_hook(self):
         await self.tree.sync()
