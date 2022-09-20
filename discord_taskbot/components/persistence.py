d811048eacd006c4c13d5e7c66a15b818511bb77
Brain4Tech <Jefta.deLeeuw@web.de>
2022-09-20 14:33:37 +0200

    correct project creation and id creating + caching

--- discord_taskbot/components/persistence.py
+++ discord_taskbot/components/persistence.py
@@ -4,7 +4,9 @@ Database component.
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from .models import Project, Task
+from .models import Project, Task, Env, ORM_BASE
+from .cache import PersistenceCache
+from sqlalchemy.engine import Engine
 
 __all__ = ['PersistenceAPI']
 
@@ -13,18 +15,57 @@ class PersistenceAPI:
     def __init__(self) -> None:
         """Class that provides an api for accessing and modifying the persistance layer."""
 
-        self._engine: None
+        self._engine: Engine
+        self._cache: PersistenceCache = PersistenceCache()
 
     def startup(self) -> None:
         """Create the database and do some startup things."""
 
-        self._engine = create_engine("sqlite://data.db")
+        print("Starting db.")
 
-    def add_project(self, name: str, description: str, channel_id: int) -> None:
+        # create engine and tables
+        self._engine = create_engine("sqlite:///data.db", echo=True)
+        ORM_BASE.metadata.create_all(self._engine)
+
+        # create env variables
+        env_vars = [
+            Env(name="PROJECT_ID_COUNT", value="0")
+        ]
+        env_dict = {k.name : k for k in env_vars}
+
+        for v in env_dict.values():
+            self._cache.add(v.name, v.value)
+
+        with Session(self._engine) as session:
+            existing_envs: list[Env] = session.query(Env).all()
+
+            # update existing env vars in cache
+            for var in list(filter(lambda x : x.name in env_dict, existing_envs)):
+                self._cache.update(var.name, var.value)
+
+            # add non-existing env vars to db
+            env_dict_copy = env_dict.copy()
+            for var in existing_envs:
+                env_dict_copy.pop(var.name, None)
+            
+            for var in env_dict_copy.values():
+                print(var)
+                session.add(var)
+
+            session.flush(); session.commit()
+
+    def add_project(self, tag:str, displayname: str, description: str, channel_id: int) -> None:
         """Create a new project."""
 
         with Session(self._engine) as session:
-            pass
+            p = Project(tag=tag, display_name=displayname, description=description, channel_id=channel_id)
+            p.id = int(self._cache.get("PROJECT_ID_COUNT")) + 1
+            session.query(Env).filter(Env.name == "PROJECT_ID_COUNT").first().value = str(p.id)
+            
+            session.add(p)
+            session.flush()
+
+            session.commit()
 
     def add_task(self, projectid, name: str, description: str) -> None:
         """Create a new task for a project."""
