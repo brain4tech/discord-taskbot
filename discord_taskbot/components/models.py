f2da835f22cfd6196df0cf0487b66c4d02d31a13
Brain4Tech <Jefta.deLeeuw@web.de>
2022-09-27 18:44:09 +0200

    set some defaults

--- discord_taskbot/components/models.py
+++ discord_taskbot/components/models.py
@@ -3,6 +3,7 @@ ORM models.
 """
 
 from contextlib import nullcontext
+from email.policy import default
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
@@ -14,8 +15,8 @@ class Project(ORM_BASE):
     """Database table to store all projects."""
     __tablename__ = 'projects'
 
-    tag = Column(String, primary_key=True)
-    id = Column(Integer, nullable=False, default=-1)
+    tag = Column(String, primary_key = True)
+    id = Column(Integer, nullable = False, default = -1)
     display_name = Column(String)
     description = Column(String)
     channel_id = Column(Integer)
@@ -25,15 +26,15 @@ class Task(ORM_BASE):
     """Database table to store all tasks."""
     __tablename__ = 'tasks'
 
-    id = Column(Integer, primary_key=True)
-    related_project = Column(Integer, ForeignKey("projects.id"), nullable = False)
-    number = Column(Integer, nullable = False)
+    id = Column(Integer, primary_key = True)
+    related_project = Column(Integer, nullable = False)
+    number = Column(Integer, nullable = False, default = -1)
     title = Column(String, nullable = False)
     description = Column(String)
     status = Column(String)
     assigned_to = Column(Integer)
-    message_id = Column(Integer, nullable = False)
-    thread_id = Column(Integer)
+    message_id = Column(Integer, nullable = False, default=-1)
+    thread_id = Column(Integer, nullable=False, default=-1)
 
 class Env(ORM_BASE):
     """Database table to store bot states."""
