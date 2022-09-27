9e97667624d4201919a4c1154782ff0df8ee8dd5
Brain4Tech <Jefta.deLeeuw@web.de>
2022-09-27 11:56:33 +0200

    update constant impot

--- discord_taskbot/utils/functions.py
+++ discord_taskbot/utils/functions.py
@@ -3,10 +3,10 @@ Functions to summarize repeatable processes.
 """
 
 import discord
-from .constants import TASK_EMOJIS
+from .constants import DEFAULT_TASK_EMOJIS
 
 async def send_new_task(channel: discord.TextChannel, title: str, description: str):
 
     message: discord.Message = await channel.send(f"**Task #{0}:** {title}\n{description}")
-    for emoji in TASK_EMOJIS:
+    for emoji in DEFAULT_TASK_EMOJIS:
         await message.add_reaction(emoji)
