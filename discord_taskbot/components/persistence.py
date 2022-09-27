7d0965d03b9564d1e9a2b2ef85cb2bc376f85702
Brain4Tech <Jefta.deLeeuw@web.de>
2022-09-27 11:58:10 +0200

    add action emoji modifying to persistence api

--- discord_taskbot/components/persistence.py
+++ discord_taskbot/components/persistence.py
@@ -4,10 +4,12 @@ Database component.
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from .models import Project, Task, Env, ORM_BASE
+from .models import Project, Task, Env, Emoji, ORM_BASE
 from .cache import PersistenceCache
 from sqlalchemy.engine import Engine
-from .exceptions import ChannelAlreadyInUse
+from .exceptions import ChannelAlreadyInUse, EmojiDoesNotExist
+
+from discord_taskbot.utils.constants import TASK_EMOJI_IDS, DEFAULT_TASK_EMOJI_MAPPING
 
 __all__ = ['PersistenceAPI']
 
@@ -24,11 +26,11 @@ class PersistenceAPI:
 
         print("Starting db.")
 
-        # create engine and tables
+        ### create engine and tables
         self._engine = create_engine("sqlite:///data.db", echo=True)
         ORM_BASE.metadata.create_all(self._engine)
 
-        # create env variables
+        ### create env variables
         env_vars = [
             Env(name="PROJECT_ID_COUNT", value="0")
         ]
@@ -53,6 +55,34 @@ class PersistenceAPI:
                 session.add(var)
 
             session.flush(); session.commit()
+
+        ### create task action emojis
+        existing_emojis = self.get_task_action_emoji_mapping()
+
+        # if query result and emoji_ids have equivalent ids, return
+        if DEFAULT_TASK_EMOJI_MAPPING.keys() == existing_emojis.keys():
+            return
+
+        # only keep those emojis which id's are valid
+        valid_existing_emoji_ids = list(filter(lambda x : x in TASK_EMOJI_IDS, existing_emojis.keys()))
+        
+        # use in-db empjis for existing ones, use default emojis for new ones
+        new_emoji_mapping = DEFAULT_TASK_EMOJI_MAPPING.copy()
+        for e in valid_existing_emoji_ids:
+            new_emoji_mapping[e] = existing_emojis[e]
+        
+        position_count = 1
+        with Session(self._engine) as session:
+
+            for e in existing_emojis:
+                session.delete(session.get(Emoji, e))
+
+            for id, emoji in new_emoji_mapping.items():
+                session.add(Emoji(id=id, emoji=emoji, position=position_count))
+                position_count += 1
+
+            session.flush(); session.commit()
+
 
     def add_project(self, tag: str, displayname: str, description: str, channel_id: int) -> None:
         """Create a new project."""
@@ -112,3 +142,22 @@ class PersistenceAPI:
                 channels.add(r[0])
         
         return channels
+
+    def update_task_action_emoji(self, id: str, emoji: str) -> None:
+        """Update a task action emoji."""
+        with Session(self._engine) as session:
+            e: Emoji = session.query(Emoji).filter(Emoji.id == id).first()
+            if not e:
+                raise EmojiDoesNotExist(f"Emoji '{id}' cannot be updated because it does not exist.")
+            
+            e.emoji = emoji
+
+            session.flush(); session.commit()
+
+
+    def get_task_action_emoji_mapping(self) -> dict[str, str]:
+        """Get all task action emoji in a map {id: emoji}."""
+        with Session(self._engine) as session:
+            emojis: list[Emoji] = session.query(Emoji).order_by(Emoji.position).all()
+        
+        return {e.id: e.emoji for e in emojis}
