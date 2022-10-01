57b4ed1043592de41ddfbe5e2c5c8a89ae75acb5
Brain4Tech <Jefta.deLeeuw@web.de>
2022-10-01 12:33:56 +0200

    improve documentation

--- discord_taskbot/components/cache.py
+++ discord_taskbot/components/cache.py
@@ -11,28 +11,35 @@ class PersistenceCache:
         """
         Add and update cache variables to speed up data access times.
         
-        It's the programmer's responsibility changes to the cache are stored in the db too.
+        It's the programmer's responsibility to syncronize cache and persistent storage.
+
+        Methods:
+            add
+            get
+            update
+            remove
+        
+        See the method docstrings for more explanation.
         """
 
     def add(self, name: str, value: Any) -> None:
         """Add a new value to the cache."""
         if name in vars(self):
-            raise AttributeError(f"Attribute '{name}' already exists. If you want to update it use .update().")
+            raise AttributeError(f"Attribute '{name}' already exists in cache. If you want to update it use .update(name, value).")
         self.__setattr__(name, value)
 
     def get(self, name: str) -> Any | None:
-        """Get a value from the cache. If value does not exist None is returned."""
+        """Get a value from the cache. If it does not exist None is returned."""
         try:
             return self.__getattribute__(name)
         except:
             return None
-
     
     def update(self, name: str, value: Any) -> None:
         """Update a value in the cache."""
         
         if name not in vars(self):
-            raise AttributeError(f"Attribute '{name}' does not exist in cache. Please add it with .add().")
+            raise AttributeError(f"Attribute '{name}' does not exist in cache. Add new values with add() before updating them.")
         self.__setattr__(name, value)
     
     def remove(self, name: str) -> None:
